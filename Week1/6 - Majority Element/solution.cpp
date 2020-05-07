1d2549684e3d2056def1bd2423e5bee3bc8338fe
Nisarg Shah <nisshah1499@gmail.com>
2020-05-07 16:27:51 +0530

    6th Ques of Week1 added

---
+++ Week1/6 - Majority Element/solution.cpp
@@ -0,0 +1,31 @@
+//
+// LeetCode : First Bad Version
+//
+// Author : Nisarg Shah (@nisarg1499)
+//
+// Web : iamnisarg.in
+
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+     
+        vector<int> copy;
+        vector<int>::iterator ip;
+        int length = nums.size();
+        length = length/2;
+        int final = 0;
+        copy = nums;
+
+        ip = unique(copy.begin(), copy.begin() + copy.size()); 
+        copy.resize(distance(copy.begin(), ip));
+
+        for(int i = copy.size()-1 ; i >= 0 ; i--){
+            if( count(nums.begin(), nums.end(), copy[i]) > length ){
+                return copy[i];
+            }
+        }
+
+        return final;
+    }
+};
