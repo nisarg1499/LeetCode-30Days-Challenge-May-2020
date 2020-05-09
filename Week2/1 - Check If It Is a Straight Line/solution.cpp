098cea83db09d4c3cd48b48d9b891ee5b3662e78
Nisarg Shah <nisshah1499@gmail.com>
2020-05-09 15:28:29 +0530

    Week2 Q1 Added

---
+++ Week2/1 - Check If It Is a Straight Line/solution.cpp
@@ -0,0 +1,29 @@
+/*
+ * LeetCode : Cousins in Binary Tree
+ *
+ * Author : Nisarg Shah (@nisarg1499)
+ *
+ * Web : iamnisarg.in
+**/
+
+
+class Solution {
+public:
+    bool checkStraightLine(vector<vector<int>>& c) {
+        
+        float slope = 0;
+        slope = ((float)c[1][1] - (float)c[0][1]) / ((float)c[1][0] - (float)c[0][0]);
+        if(c.size() == 2){
+            return true;
+        }
+        else{
+            for(int i = 2 ; i < c.size() ; i++){
+                if( ((float)c[i][1] - (float)c[i-1][1]) / ((float)c[i][0] - (float)c[i-1][0]) != slope ){
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+    }
+};
